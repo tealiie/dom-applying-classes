82a94d94473109827195fc08dd10694231c57d89
tealiie <tealiie@live.com>
2016-08-10 01:27:09 +1200

    change div colour and make 4th div visible

--- classes.js
+++ classes.js
@@ -1,12 +1,15 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one()
+  two()
+  three()
+  makeVisible()
+
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -19,7 +22,19 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
+function two () {
+  var two = document.getElementById('two')
+  two.classList.add('green')
+}
 
 // CREATE FUNCTION three HERE
+function three () {
+  var three = document.getElementById('three')
+  three.classList.add('pink')
+}
 
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+  var makeVisible = getElementsByClassName('invisible')
+  makeVisible.classList.add('visible')
+}
